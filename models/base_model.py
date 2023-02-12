51c0a10594b81675f6ec51a2aa5ecd688dd7ff2b
odai307 <gabrielafotey@gmail.com>
2023-02-12 23:52:32 +0000

    updated base_model

--- models/base_model.py
+++ models/base_model.py
@@ -6,11 +6,20 @@ from datetime import datetime
 
 class BaseModel:
     """A BaseModel class"""
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Initializing the class"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at":
+                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                if key == "updated_at":
+                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                if key != "__class__":
+                    setattr(self, key, value)                
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """Print the representation of the BaseModel class"""
